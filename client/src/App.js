9dd8d0b7514f63c82b59efe1e71541d9e48b3d5e
Mohamed Elzohary <MohammadBahaa_2001mb@outlook.com>
2020-10-12 12:01:15 +0200

    client-side init files: React

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// components
+import BookList from "./components/BookList"
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <h1>Book Shelf</h1>
+        <BookList></BookList>
+      </div>
+    );
+  }
 }
 
 export default App;
