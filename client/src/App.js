0bee6732c7adaa3613b7ecfdf09640e09a3bc900
Mohamed Elzohary <MohammadBahaa_2001mb@outlook.com>
2020-10-12 12:07:39 +0200

    client-side init files: Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 // components
 import BookList from "./components/BookList"
 
+// apollo client setup
+const client = new ApolloClient({
+  uri: "https://localhost:8000/___graphql"
+})
+
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Book Shelf</h1>
-        <BookList></BookList>
-      </div>
+      <ApolloProvider client={client}>
+
+        <div id="main">
+          <h1>Book Shelf</h1>
+          <BookList></BookList>
+        </div>
+
+      </ApolloProvider>
     );
   }
 }
