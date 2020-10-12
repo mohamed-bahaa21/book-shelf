034ec303972f95de1371cf2eb0e084c729945ef9
Mohamed Elzohary <MohammadBahaa_2001mb@outlook.com>
2020-10-12 16:55:42 +0200

    Book Details Component + Making a Single Query

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { graphql } from "react-apollo";
 
-import { getBookQuery } from "../queries/queries";
+import { getBooksQuery } from "../queries/queries";
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selected: null
+        }
+    }
 
     displayBooks = () => {
         var data = this.props.data
@@ -12,7 +19,7 @@ class BookList extends Component {
         } else {
             return data.books.map(book => {
                 return (
-                    <li>{book.name}</li>
+                    <li key={book.id} onClick={(e) => { this.setState({ selected: book.id }) }}>{book.name}</li>
                 )
             })
         }
@@ -24,9 +31,10 @@ class BookList extends Component {
                 <ul id="book-list">
                     {this.displayBooks()}
                 </ul>
+                <BookDetails bookId={this.state.selected} />
             </div>
         );
     }
 }
 
-export default graphql(getBookQuery)(BookList);
+export default graphql(getBooksQuery)(BookList);
--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -1,6 +1,6 @@
 import { gql } from "apollo-boost";
 
-const getBookQuery = gql`
+const getBooksQuery = gql`
     {
         books {
             name
@@ -26,4 +26,23 @@ const addBookMutation = gql`
     }
 `
 
-export { getBookQuery, getAuthorsQuery, addBookMutation }
+const getBookQuery = gql`
+    query($id: ID){
+        book(id: $id){
+            id
+            name
+            genre
+            author{
+                id
+                name
+                age
+                books {
+                    name
+                    id
+                }
+            }
+        }
+    }
+`
+
+export { getBooksQuery, getAuthorsQuery, addBookMutation, getBookQuery }
